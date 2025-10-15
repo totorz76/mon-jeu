2e069117cc4e9c5e285d905f477c934e834c3a78
totorz76 <victor76.zammit@gmail.com>
2025-10-15 11:48:51 +0200

    Règles + résultat

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,6 @@ function powerPc(){
     return element[index];
 }
 powerPc()
-console.log(powerPc());
 
 
 const buttons = document.querySelectorAll("button");
@@ -18,5 +17,26 @@ buttons.forEach((button) => {
 })
 
 function powerPlayer(userChoice){
+    // au moment de notre choix il faut que le pc choisisse (donc la const en dessous), puis on annonce les choix et après on appelle la fonction qui permet de définir les règles du jeu
+    const pcChoice = powerPc()
     console.log("Le joueur a choisi :", userChoice);
+    console.log("L'ordinateur a choisi :", pcChoice);
+    playGame (pcChoice, userChoice)    
 } 
+
+
+
+function playGame(pcChoice, userChoice){
+    if (pcChoice === userChoice){
+        console.log('Egalité !');
+    }
+    else if (
+        (userChoice === "feu" && pcChoice === "terre") ||
+        (userChoice === "eau" && pcChoice === "feu") ||
+        (userChoice === "terre" && pcChoice === "feu")
+    ) {
+        console.log('Le Joueur a gagné !');
+    } else {
+        console.log("L'Ordinateur a gagné !");
+    }
+}
