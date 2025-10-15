4bb6287b0da3080a9f34474a8ce4b1b31161bef9
totorz76 <victor76.zammit@gmail.com>
2025-10-15 12:13:41 +0200

    score système

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 
+let scoreJoueur = 0;
+let scoreOrdi = 0;
 
 function powerPc(){
     const element = ["feu", "eau" , "terre"]
@@ -6,7 +8,6 @@ function powerPc(){
     return element[index];
 }
 powerPc()
-
 
 const buttons = document.querySelectorAll("button");
 
@@ -24,19 +25,27 @@ function powerPlayer(userChoice){
     playGame (pcChoice, userChoice)    
 } 
 
-
-
 function playGame(pcChoice, userChoice){
+    let resultat = "";
     if (pcChoice === userChoice){
+        resultat = "Egalité !"
         console.log('Egalité !');
     }
     else if (
         (userChoice === "feu" && pcChoice === "terre") ||
         (userChoice === "eau" && pcChoice === "feu") ||
-        (userChoice === "terre" && pcChoice === "feu")
+        (userChoice === "terre" && pcChoice === "eau")
     ) {
+         resultat = "Le Joueur a gagné !"
+         scoreJoueur++;
         console.log('Le Joueur a gagné !');
     } else {
+         resultat = "L'Ordinateur a gagné !"
+         scoreOrdi++;
         console.log("L'Ordinateur a gagné !");
     }
+    document.getElementById("resultat").textContent = "Résultat : "+ resultat;
+
+    document.getElementById("score-joueur").textContent = scoreJoueur
+    document.getElementById("score-ordi").textContent = scoreOrdi
 }
